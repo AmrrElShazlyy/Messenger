04182694f553efce1c6bdd69bbc419c0821f2999
amro-mostafaelshazly <amro.mostafaelshazly@vodafone.com>
2022-08-28 17:05:29 +0200

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Amr El Shazly on 28/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
