083784cd6cfb3a5cb355e468495a0dac16bfb3a3
amro-mostafaelshazly <amro.mostafaelshazly@vodafone.com>
2022-08-28 17:42:20 +0200

    adding View Controllers , pod file

---
+++ Messenger/Controllers/NewConversationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewConversationsViewController.swift
+//  Messenger
+//
+//  Created by Amr El Shazly on 28/08/2022.
+//
+
+import UIKit
+
+class NewConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
